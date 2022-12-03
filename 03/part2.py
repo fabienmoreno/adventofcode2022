0db9d267299c560411c71d916121e841966949a4
fabienmoreno <44601636+fabienmoreno@users.noreply.github.com>
2022-12-04 00:09:02 +0100

    03 part 2

---
+++ 03/part2.py
@@ -0,0 +1,17 @@
+f=open('03/input.txt','r')
+l=f.read().split("\n")
+p=int(len(l))
+import string
+s=0
+for i in range(0,p,3):
+    a=l[i]
+    b=l[i+1]
+    c=l[i+2]
+    for j in a:
+        B=b.count(j)
+        C=c.count(j)
+        if B>=1 and C>=1:
+            s+=string.ascii_letters.index(j)+1
+            #print(j, string.ascii_letters.index(j)+1)
+            break
+print(s)
